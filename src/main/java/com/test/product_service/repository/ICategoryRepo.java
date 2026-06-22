00eed5ebf702516ca0a37af39924c23ec5d9fd19
namanparashar <namanparashar484@gmail.com>
2026-06-22 18:16:10 +0530

    method findAll with Pageabl is already there

--- src/main/java/com/test/product_service/repository/ICategoryRepo.java
+++ src/main/java/com/test/product_service/repository/ICategoryRepo.java
@@ -1,12 +1,7 @@
 package com.test.product_service.repository;
 
 import com.test.product_service.entity.Category;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ICategoryRepo extends JpaRepository<Category, Integer> {
-
-    @Override
-    Page<Category> findAll(Pageable pageable);
 }
--- src/main/java/com/test/product_service/repository/IProductRepo.java
+++ src/main/java/com/test/product_service/repository/IProductRepo.java
@@ -1,13 +1,8 @@
 package com.test.product_service.repository;
 
 import com.test.product_service.entity.Product;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface IProductRepo extends JpaRepository<Product, Integer> {
-
-    @Override
-    Page<Product> findAll(Pageable pageable);
 
 }
